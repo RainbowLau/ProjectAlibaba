14d82f79b39551c928749ae7c5d95aa8bab23fbf
Rainbowlau <h57251180@gmail.com>
2021-03-25 23:10:09 +0800

    创建项目

---
+++ MybatisPlus/src/main/java/com/cloud/MybatisPlusApplication.java
@@ -0,0 +1,18 @@
+package com.cloud;
+
+import com.sun.glass.ui.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright (C)
+ * Author:   mino
+ * Date:     25/3/21 11:05 下午
+ * History:
+ */
+@SpringBootApplication
+public class MybatisPlusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class,args);
+    }
+}
